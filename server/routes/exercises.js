f6cefb96a844399ad8f864ee8180077b9f1b5413
michaelliann <cafebubs@gmail.com>
2022-01-02 23:38:49 -0800

    Add update route

--- server/routes/exercises.js
+++ server/routes/exercises.js
@@ -31,26 +31,74 @@ router.get('/log', authenticate, async (req, res) => {
 // Save a new exercise
 router.post('/log', authenticate, async (req, res) => {
     try {
-        const { exerciseName, exercisePR, exercisePRHistory, exerciseGoal } = req.body;
+        const { exerciseName, exercisePR, exerciseHistory, exerciseGoal } = req.body;
 
         // Create a new exercise
         const newExercise = new Exercise({
             user: req.user,
             exerciseName,
             exercisePR,
-            exercisePRHistory,
+            exerciseHistory,
             exerciseGoal
         });
+
+        // TODO: Loop through history and add counters
 
         // Save the exercise to db
         const savedExercise = await newExercise.save();
         res.json(savedExercise);
-    } catch (err) {
+    }
+    catch (err) {
         console.log(err);
         res.status(500).send();
     }
 });
 
 // TODO: update document route
+router.post('/log/update', authenticate, async (req, res) => {
+    try {
+        // Front end gives exercise name
+        const { exerciseName, exercisePR, exerciseHistory, exerciseGoal } = req.body;
+
+        // Check for PR in history and update PR
+
+        // Add counter if history is updated
+
+        // Save the new data
+    }
+    catch (err) {
+
+    }
+});
+
+// Delete a user's exercise
+router.delete('/log/delete', authenticate, async (req, res) => {
+    try {
+        const { exerciseName } = req.body;
+
+        const deletedExercise = await Exercise.findOneAndDelete({ user: req.user, exerciseName });
+
+        if (!deletedExercise) {
+            return res.status(400).json({
+                success: false,
+                errorMessage: "Exercise does not exist!",
+                exercise: null
+            });
+        }
+        
+        res.json({
+            success: true,
+            errorMessage: null,
+            exercise: deletedExercise
+        });
+    }
+    catch (err) {
+        res.status(400).json({
+            success: false,
+            errorMessage: "An error has occurred",
+            exercise: null
+        })
+    }
+});
 
 module.exports = router;
