6ef5f49f06a93c5903f08f9ec9ce530bc05f7b14
michaelliann <cafebubs@gmail.com>
2022-01-02 23:36:47 -0800

    Added axios withCredentials option for cookies

--- client/src/App.js
+++ client/src/App.js
@@ -12,10 +12,17 @@ import Register from "./components/Register";
 import React, { Component } from "react";
 import bcrypt from "bcryptjs";
 import axios from "axios";
+import { Exercisepicker } from "./components/exercisepicker/Exercisepicker";
+import { Trackerchart } from "./components/charts/Trackerchart";
+import { ExerciseHistory } from "./components/ExerciseHistory/ExerciseHistory";
+import { Navbar } from "react-bootstrap";
+import { MyNav } from "./components/navbar";
+
+// VERY IMPORTANT
+axios.defaults.withCredentials = true;
 
 const salt = bcrypt.genSaltSync(10);
 const BaseURL = "http://localhost:5000";
-
 class App extends Component {
   constructor(props) {
     super(props);
@@ -28,6 +35,27 @@ class App extends Component {
         // Other fields maybe applied
       },
       ErrorMessage: "",
+      CurrentExercise: "",
+      accessToken: "",
+      graphData: {
+        labels: ["test1", "test2", "test3", "4"],
+        datasets: [
+          {
+            label: "History",
+            data: [20, 200, 140],
+            fill: true,
+            backgroundColor: "rgba(75,192,192,0.2)",
+            borderColor: "rgba(75,192,192,1)",
+          },
+          {
+            label: "Goal",
+            data: [500, 500, 500, 500],
+            fill: true,
+            backgroundColor: "rgba(75,192,192,0.2)",
+            borderColor: "rgba(75,192,192,1)",
+          },
+        ],
+      },
     };
     this.handleRegister = this.handleRegister.bind(this);
   }
@@ -36,7 +64,7 @@ class App extends Component {
     // Encrypt the password and send it to server
     const hashedPassword = bcrypt.hashSync(password, salt);
     axios
-      .post(BaseURL + "/register", {
+      .post(BaseURL + "/users/register", {
         email: email,
         username: username,
         name: name,
@@ -51,7 +79,7 @@ class App extends Component {
           });
         } else {
           this.setState({
-            ErrorMessage: "The username has already be taken!",
+            ErrorMessage: "The username has already been taken!",
           });
         }
       });
@@ -60,13 +88,12 @@ class App extends Component {
   handleLogin = (username, password) => {
     const hashedPassword = bcrypt.hashSync(password, salt);
     axios
-      .post(BaseURL + "/login", {
+      .post(BaseURL + "/users/login", {
         username: username,
         password: password,
-      }, {
-        withCredentials: true // httpOnly cookie implementation
       })
       .then((response) => {
+        console.log(response);
         if (response.data.success === true) {
           const user = response.data.user;
           this.setState({
@@ -81,15 +108,92 @@ class App extends Component {
       });
   };
 
+  handleSelect = (e) => {
+    this.setState({ CurrentExercise: e });
+    this.getData(e, this.state.accessToken);
+  };
+
+  formatDate = (date) => {
+    var options = {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    };
+    return new Date(date).toLocaleDateString("en-US", options);
+  };
+  getData = (exercise, token) => {
+    axios
+      .get("http://localhost:5000/exercises/log")
+      .then((response) => {
+        const data = {
+          labels: [],
+          datasets: [
+            {
+              label: "History",
+              data: [],
+              fill: true,
+              backgroundColor: "rgba(75,192,192,0.2)",
+              borderColor: "rgba(75,192,192,1)",
+            },
+            {
+              label: "Goal",
+              data: [],
+              fill: true,
+              backgroundColor: "rgba(75,192,192,0.2)",
+              borderColor: "rgba(75,192,192,1)",
+            },
+          ],
+        };
+        data.datasets[0].data = [];
+        data.datasets[1].data = [];
+        response.data.forEach((element) => {
+          //let exercise = element.json()
+          var options = {
+            weekday: "long",
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+          };
+          if (element.exerciseName === exercise) {
+            //if picked exercise matches exercise from data, push date to labels, data to history, as well as goal
+            element.exercisePRHistory.forEach((pr) => {
+              data.labels.push(this.formatDate(pr.date.toString()));
+              data.datasets[0].data.push(pr.value);
+              data.datasets[1].data.push(element.exerciseGoal);
+            });
+            data.datasets[0].data.push(element.exercisePR.value);
+            data.labels.push(
+              this.formatDate(element.exercisePR.date.toString())
+            );
+            data.datasets[1].data.push(element.exerciseGoal);
+            this.setState({ graphData: data });
+            console.log("marker2");
+          }
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  logOut = () => {
+    this.setState({
+      IsLoggedIn: false,
+      User: {},
+      accessToken: "",
+    });
+  };
+
   render() {
     const loggedIn = this.state.IsLoggedIn;
     return (
       <Router>
         <Switch>
           <Route exact path="/">
-            <Home />
+            <Home loggedIn={this.state.IsLoggedIn} logOut={this.logOut} />
           </Route>
-          <Route exact path="/login">
+          <Route exact path="/users/login">
             {loggedIn ? (
               <Redirect to="/" />
             ) : (
@@ -100,7 +204,20 @@ class App extends Component {
               />
             )}
           </Route>
-          <Route exact path="/register">
+          <Route exact path="/exercises/log">
+            {loggedIn ? (
+              <>
+                <MyNav loggedIn={this.state.IsLoggedIn} logOut={this.logOut} />
+                <ExerciseHistory
+                  handleSelect={this.handleSelect}
+                  graphData={this.state.graphData}
+                />
+              </>
+            ) : (
+              <Redirect to="/" />
+            )}
+          </Route>
+          <Route exact path="/users/register">
             {loggedIn ? (
               <Redirect to="/" />
             ) : (
