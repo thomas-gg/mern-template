0a11e138702f97d63ae9d3cbccbe32ee4dc53ce1
Justin B <40068612+fantasticfishman@users.noreply.github.com>
2022-01-08 23:22:57 -0800

    added more exercises to exercisepicker
    kept run and swim just for testing purposes

--- client/src/components/exercisepicker/Exercisepicker.js
+++ client/src/components/exercisepicker/Exercisepicker.js
@@ -13,6 +13,15 @@ export const Exercisepicker = (props) => {
                 <Dropdown.Item eventKey="Deadlift">Deadlift</Dropdown.Item>
                 <Dropdown.Item eventKey="Squat">Squat</Dropdown.Item>
                 <Dropdown.Item eventKey="Bench">Bench</Dropdown.Item>
+                <Dropdown.Item eventKey="Barbell Row">Barbell Row</Dropdown.Item>
+                <Dropdown.Item eventKey="Shoulder Press">Shoulder Press</Dropdown.Item>
+                <Dropdown.Item eventKey="Pull Ups">Pull Ups</Dropdown.Item>
+                <Dropdown.Item eventKey="Dumbell Bench Press">Dumbbell Bench Press</Dropdown.Item>
+                <Dropdown.Item eventKey="Dumbbell Curl">Pull Ups</Dropdown.Item>
+                <Dropdown.Item eventKey="Barbell Curl">Pull Ups</Dropdown.Item>
+                <Dropdown.Item eventKey="Bent Over Row">Pull Ups</Dropdown.Item>
+                <Dropdown.Item eventKey="Front Squat">Pull Ups</Dropdown.Item>
+                <Dropdown.Item eventKey="Bent Over Row">Pull Ups</Dropdown.Item>
             </Dropdown.Menu>
         </Dropdown>
     );
